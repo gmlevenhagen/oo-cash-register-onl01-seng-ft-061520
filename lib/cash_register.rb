0391c80f1a70dab0a9f8b77e2c607ddd91921416
Garett Levenhagen <garettlevenhagen@gmail.com>
2020-06-29 12:31:44 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -9,16 +9,11 @@ class CashRegister
     end
 
     def add_item(item, price, quantity = 1)
-      @price = price
-      @total += price * quantity
-      if quantity > 1
-        counter = 0
-        while counter < quantity
-          @items << item
-          counter += 1
-        end
-      else
-        @items << item
+      self.total += price * quantity
+      quantity.times do
+        items << item
+      end
+      self.transaction = amount * quantity
       end
     end
 
