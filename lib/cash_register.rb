2b752e5473bf26665278a2821f98068afbf81086
Garett Levenhagen <garettlevenhagen@gmail.com>
2020-06-29 12:24:51 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -9,10 +9,18 @@ class CashRegister
     end
 
     def add_item(item, price, quantity = 1)
-      self.total += price * quantity
-      quantity.times { self.items << title }
-      self.last_transaction = [title, price, quantity]
-end
+      @price = price
+      @total += price * quantity
+      if quantity > 1
+        counter = 0
+        while counter < quantity
+          @items << item
+          counter += 1
+        end
+      else
+        @items << item
+      end
+    end
 
     def apply_discount
         if @discount == 0
